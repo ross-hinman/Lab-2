17af558cd3980e7e9e2d6406c48c390bd80e58c6
Ross Hinman <hinman.ross@gmail.com>
2018-07-16 14:01:32 -0400

    Lab 2 initial commit

---
+++ Lab 2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_2
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            double length;
+            double width;
+            double height;
+            string answer = "y";
+
+
+            while (answer == "y")
+            { 
+            Console.WriteLine("Enter Length: ");
+            length = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter Width: ");
+            width = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Enter Height: ");
+                height = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Area: " + length * width);
+            Console.WriteLine("Perimeter: " + 2 * length + 2 * width);
+                Console.WriteLine("Volume: " + length * width * height);
+                Console.WriteLine("Continue? (y/n)");
+                answer = (Console.ReadLine());
+                answer = answer.ToLower();
+            }
+        }
+       
+
+        }  
+    }
+
